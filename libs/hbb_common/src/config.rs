8be55aa3214f393e2af0ed47658c8979c4c3b723
hongjundu <hongjun.du.cn@gmail.com>
2023-06-07 18:20:18 +0800

    modify code, build passed on windows platform cargo 1.67.0

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -656,7 +656,7 @@ const PEERS: &str = "peers";
 
 impl PeerConfig {
     pub fn load(id: &str) -> PeerConfig {
-        let _ = CONFIG.read().unwrap(); // for lock
+        let _unused = CONFIG.read().unwrap(); // for lock
         match confy::load_path(&Self::path(id)) {
             Ok(config) => config,
             Err(err) => {
@@ -667,7 +667,7 @@ impl PeerConfig {
     }
 
     pub fn store(&self, id: &str) {
-        let _ = CONFIG.read().unwrap(); // for lock
+        let _unused = CONFIG.read().unwrap(); // for lock
         if let Err(err) = confy::store_path(Self::path(id), self) {
             log::error!("Failed to store config: {}", err);
         }
@@ -808,7 +808,7 @@ pub struct LanPeers {
 
 impl LanPeers {
     pub fn load() -> LanPeers {
-        let _ = CONFIG.read().unwrap(); // for lock
+        let _unused = CONFIG.read().unwrap(); // for lock
         match confy::load_path(&Config::file_("_lan_peers")) {
             Ok(peers) => peers,
             Err(err) => {
